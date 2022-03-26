dbdeadc97a8472961e58ad45672f00564a5a2e1e
Aman Pal <89698132+12345678-cyber@users.noreply.github.com>
2022-03-26 17:39:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,67 @@
-import { isNodePattern } from '@jimp/utils';
+import { isNodePattern, throwError } from '@jimp/utils';
 
 /**
- * Creates a circle out of an image.
- * @param {function(Error, Jimp)} options (optional) radius, x, y
+ * Scale the image so the given width and height keeping the aspect ratio. Some parts of the image may be clipped.
+ * @param {number} w the width to resize the image to
+ * @param {number} h the height to resize the image to
+ * @param {number} alignBits (optional) A bitmask for horizontal and vertical alignment
+ * @param {string} mode (optional) a scaling method (e.g. Jimp.RESIZE_BEZIER)
  * @param {function(Error, Jimp)} cb (optional) a callback for when complete
  * @returns {Jimp} this for chaining of methods
  */
 export default () => ({
-  circle(options = {}, cb) {
-    if (typeof options === 'function') {
-      cb = options;
-      options = {};
+  cover(w, h, alignBits, mode, cb) {
+    if (typeof w !== 'number' || typeof h !== 'number') {
+      return throwError.call(this, 'w and h must be numbers', cb);
     }
 
-    const radius =
-      options.radius ||
-      (this.bitmap.width > this.bitmap.height
-        ? this.bitmap.height
-        : this.bitmap.width) / 2;
-
-    const center = {
-      x: typeof options.x === 'number' ? options.x : this.bitmap.width / 2,
-      y: typeof options.y === 'number' ? options.y : this.bitmap.height / 2
-    };
-
-    this.scanQuiet(0, 0, this.bitmap.width, this.bitmap.height, function(
-      x,
-      y,
-      idx
+    if (
+      alignBits &&
+      typeof alignBits === 'function' &&
+      typeof cb === 'undefined'
     ) {
-      const curR = Math.sqrt(
-        Math.pow(x - center.x, 2) + Math.pow(y - center.y, 2)
+      cb = alignBits;
+      alignBits = null;
+      mode = null;
+    } else if (typeof mode === 'function' && typeof cb === 'undefined') {
+      cb = mode;
+      mode = null;
+    }
+
+    alignBits =
+      alignBits ||
+      this.constructor.HORIZONTAL_ALIGN_CENTER |
+        this.constructor.VERTICAL_ALIGN_MIDDLE;
+    const hbits = alignBits & ((1 << 3) - 1);
+    const vbits = alignBits >> 3;
+
+    // check if more flags than one is in the bit sets
+    if (
+      !(
+        (hbits !== 0 && !(hbits & (hbits - 1))) ||
+        (vbits !== 0 && !(vbits & (vbits - 1)))
+      )
+    )
+      return throwError.call(
+        this,
+        'only use one flag per alignment direction',
+        cb
       );
 
-      if (radius - curR <= 0.0) {
-        this.bitmap.data[idx + 3] = 0;
-      } else if (radius - curR < 1.0) {
-        this.bitmap.data[idx + 3] = 255 * (radius - curR);
-      }
-    });
+    const alignH = hbits >> 1; // 0, 1, 2
+    const alignV = vbits >> 1; // 0, 1, 2
+
+    const f =
+      w / h > this.bitmap.width / this.bitmap.height
+        ? w / this.bitmap.width
+        : h / this.bitmap.height;
+    this.scale(f, mode);
+    this.crop(
+      ((this.bitmap.width - w) / 2) * alignH,
+      ((this.bitmap.height - h) / 2) * alignV,
+      w,
+      h
+    );
 
     if (isNodePattern(cb)) {
       cb.call(this, null, this);
