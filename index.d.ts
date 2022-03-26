c19743b8f9aad92d0d9884ec96ad5316f9ba2162
Aman Pal <89698132+12345678-cyber@users.noreply.github.com>
2022-03-26 17:42:55 +0530

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,8 @@
 import { ImageCallback } from '@jimp/core';
 
-interface Cover {
-  cover(w: number, h: number, cb?: ImageCallback<this>): this;
-  cover(w: number, h: number, alignBits?: number, cb?: ImageCallback<this>): this;
-  cover(
-    w: number,
-    h: number,
-    alignBits?: number,
-    mode?: string,
-    cb?: ImageCallback<this>
-  ): this;
+interface Dither {
+  dither565(cb?: ImageCallback<this>): this;
+  dither16(cb?: ImageCallback<this>): this;
 }
 
-export default function(): Cover;
+export default function(): Dither;
--- index.js
+++ index.js
@@ -1,72 +1,38 @@
-import { isNodePattern, throwError } from '@jimp/utils';
+import { isNodePattern } from '@jimp/utils';
 
 /**
- * Scale the image so the given width and height keeping the aspect ratio. Some parts of the image may be clipped.
- * @param {number} w the width to resize the image to
- * @param {number} h the height to resize the image to
- * @param {number} alignBits (optional) A bitmask for horizontal and vertical alignment
- * @param {string} mode (optional) a scaling method (e.g. Jimp.RESIZE_BEZIER)
+ * Apply a ordered dithering effect
  * @param {function(Error, Jimp)} cb (optional) a callback for when complete
  * @returns {Jimp} this for chaining of methods
  */
-export default () => ({
-  cover(w, h, alignBits, mode, cb) {
-    if (typeof w !== 'number' || typeof h !== 'number') {
-      return throwError.call(this, 'w and h must be numbers', cb);
-    }
-
-    if (
-      alignBits &&
-      typeof alignBits === 'function' &&
-      typeof cb === 'undefined'
-    ) {
-      cb = alignBits;
-      alignBits = null;
-      mode = null;
-    } else if (typeof mode === 'function' && typeof cb === 'undefined') {
-      cb = mode;
-      mode = null;
-    }
-
-    alignBits =
-      alignBits ||
-      this.constructor.HORIZONTAL_ALIGN_CENTER |
-        this.constructor.VERTICAL_ALIGN_MIDDLE;
-    const hbits = alignBits & ((1 << 3) - 1);
-    const vbits = alignBits >> 3;
-
-    // check if more flags than one is in the bit sets
-    if (
-      !(
-        (hbits !== 0 && !(hbits & (hbits - 1))) ||
-        (vbits !== 0 && !(vbits & (vbits - 1)))
-      )
-    )
-      return throwError.call(
-        this,
-        'only use one flag per alignment direction',
-        cb
-      );
-
-    const alignH = hbits >> 1; // 0, 1, 2
-    const alignV = vbits >> 1; // 0, 1, 2
-
-    const f =
-      w / h > this.bitmap.width / this.bitmap.height
-        ? w / this.bitmap.width
-        : h / this.bitmap.height;
-    this.scale(f, mode);
-    this.crop(
-      ((this.bitmap.width - w) / 2) * alignH,
-      ((this.bitmap.height - h) / 2) * alignV,
-      w,
-      h
+function dither(cb) {
+  const rgb565Matrix = [1, 9, 3, 11, 13, 5, 15, 7, 4, 12, 2, 10, 16, 8, 14, 6];
+  this.scanQuiet(0, 0, this.bitmap.width, this.bitmap.height, function(
+    x,
+    y,
+    idx
+  ) {
+    const thresholdId = ((y & 3) << 2) + (x % 4);
+    const dither = rgb565Matrix[thresholdId];
+    this.bitmap.data[idx] = Math.min(this.bitmap.data[idx] + dither, 0xff);
+    this.bitmap.data[idx + 1] = Math.min(
+      this.bitmap.data[idx + 1] + dither,
+      0xff
     );
+    this.bitmap.data[idx + 2] = Math.min(
+      this.bitmap.data[idx + 2] + dither,
+      0xff
+    );
+  });
 
-    if (isNodePattern(cb)) {
-      cb.call(this, null, this);
-    }
-
-    return this;
+  if (isNodePattern(cb)) {
+    cb.call(this, null, this);
   }
+
+  return this;
+}
+
+export default () => ({
+  dither565: dither,
+  dither16: dither
 });
